05ac24ec659bdcc36913f13bd1bb199b1d144d36
Nathan Lallemand <nlallema@z2r10p7.42lyon.fr>
2025-12-10 12:26:57 +0100

    add linked list

--- push_swap/includes/linked_list.h
+++ push_swap/includes/linked_list.h
@@ -6,12 +6,14 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 11:07:54 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/12/10 11:14:19 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/12/10 12:12:05 by nlallema         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LINKED_LIST_H
 # define LINKED_LIST_H
+
+# include <stdlib.h>
 
 typedef struct s_node
 {
@@ -20,8 +22,9 @@ typedef struct s_node
 	int		value;
 }			t_node;
 
-t_node	*list_front(int value, t_node **root);
-t_node	*list_back(int value, t_node **root);
+t_node	*node_new(int value);
+short	list_front(int value, t_node **root);
+short	list_back(int value, t_node **root);
 void	list_clear(t_node **root);
 
 #endif
