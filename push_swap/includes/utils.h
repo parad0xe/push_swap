8d1afdbc01b59f87661167d5c5537bd24b9a0477
leonardecavele <leonard.decavele@gmail.com>
2025-12-10 14:44:00 +0100

    edit: 'printf' is now 'dprintf' and accept 'fd' usage

--- push_swap/includes/utils.h
+++ push_swap/includes/utils.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 21:09:29 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/10 17:12:10 by ldecavel         ###   ########lyon.fr   */
+/*   Updated: 2025/12/10 14:38:57 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 # include <stdint.h>
 # include <unistd.h>
 
-typedef struct s_printf
+typedef struct s_dprintf
 {
 	va_list	param;
 	int		n;
 	int		current;
-}			t_printf;
+}			t_dprintf;
 
-int		ft_printf(const char *s, ...) __attribute__((format(printf, 1, 2)));
+int		ft_dprintf(const char *s, ...) __attribute__((format(printf, 1, 2)));
 void	print_stack(t_stack *stack);
 
 #endif
