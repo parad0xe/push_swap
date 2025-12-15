83e11726bf6eeba8fd480d92cf0ad9a2f853298c
Nathan Lallemand <nlallema@z2r10p7.42lyon.fr>
2025-12-15 14:17:44 +0100

    optimization of medium algorithm
    better pre-sort of stack B when rotate A

--- mandatory/srcs/algorithms/medium.c
+++ mandatory/srcs/algorithms/medium.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 15:01:27 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/12/13 19:21:27 by nlallema         ###   ########.fr       */
+/*   Updated: 2025/12/15 14:13:10 by nlallema         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,16 +43,18 @@ static void	pushb_next_bucket(t_stack *stack, t_info *info, int bucket_size)
 	bucket_range(stack, bucket_size, &range_min, &range_max);
 	stack_size = stack->size_a;
 	root = stack->a->previous;
-	while (stack_size--)
+	while (stack_size-- >= 0)
 	{
 		if (root->value >= range_min && root->value <= range_max)
 			break ;
 		root = root->previous;
 	}
-	while (stack_size--)
+	while (stack_size-- >= 0)
 	{
 		if (stack->a->value >= range_min && stack->a->value <= range_max)
 			pb(stack, info);
+		else if (stack->size_b > 1 && stack->b->value < stack->b->next->value)
+			rr(stack, info);
 		else
 			ra(stack, info);
 	}
@@ -66,8 +68,13 @@ extern void	medium(t_stack *stack, t_info *info)
 	num_buckets = ft_approximate_sqrt(stack->size);
 	bucket_size = ft_ceil((double)stack->size / num_buckets);
 	while (num_buckets--)
+	{
 		pushb_next_bucket(stack, info, bucket_size);
+		//print_stack(stack);
+		//exit(1);
+	}
+	//print_stack(stack);
 	while (stack->size_b)
 		pusha_max(stack, info);
-	print_stack(stack);
+	//print_stack(stack);
 }
